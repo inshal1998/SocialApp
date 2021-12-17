deefc598e9bef904eaf2372d675f0c1d0d9482ac
Inshal Ansari <inshalansari80@gmail.com>
2021-12-17 23:45:57 +0530

    Custom Components Added

---
+++ app/components/FormButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { windowHeight , windowWidth } from "../utils/Dimensions";
+const FormButton = ({buttonTitle , ...rest}) => {
+    return (
+            <TouchableOpacity style={styles.btnContainer} {...rest}>
+                <Text style={styles.btnText}>{buttonTitle}</Text>
+            </TouchableOpacity>
+    )
+}
+
+export default FormButton
+
+const styles = StyleSheet.create({
+    btnContainer :{
+        marginTop: 10,
+        width:'100%',
+        height:windowHeight/15,
+        backgroundColor:"#2e64e5",
+        padding:10,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:5
+    },
+    btnText : {
+        fontSize:18,
+        fontWeight:'bold',
+        color:'#ffffff',
+        fontFamily:'Lato-Regular'
+    }
+})
